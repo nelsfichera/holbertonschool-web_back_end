b6aae0c044008eda62e5dae70725b8cbb6aeb79a
Nels <nelsfichera@gmail.com>
2022-02-01 23:17:37 -0500

    0-9

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Basic annotations """
+
+
+def add(a: float, b: float) -> float:
+    """ static typed function takes in two floats and return the sum """
+    return a + b
