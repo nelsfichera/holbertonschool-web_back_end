b6b3535ee3e790b872769182552a67ff3f1dec13
Nels <nelsfichera@gmail.com>
2022-02-08 19:34:48 -0500

    more changes

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -5,6 +5,19 @@ from math import ceil
 from typing import List, Tuple
 
 
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Returns a tuple of size two containing: a start index
+        and an end index, corresponding to the range of indices
+        to return in a list for those
+        particular pagination parameters
+    """
+
+    start = (page - 1) * page_size
+    end = page * page_size
+
+    return (start, end)
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -72,16 +85,3 @@ class Server:
         }
 
         return hyper
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Returns a tuple of size two containing: a start index
-        and an end index, corresponding to the range of indices
-        to return in a list for those
-        particular pagination parameters
-    """
-
-    start = (page - 1) * page_size
-    end = page * page_size
-
-    return (start, end)
