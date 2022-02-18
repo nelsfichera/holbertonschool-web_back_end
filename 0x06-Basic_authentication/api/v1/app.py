504f0866f2a4629037937e2240f8367344b550a9
Nels <nelsfichera@gmail.com>
2022-02-18 15:32:46 -0500

    remove main

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -66,7 +66,8 @@ def before_request() -> str:
         abort(403)
 
 
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+'''
